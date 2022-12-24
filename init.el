54824cb637e63a0856d21225fe696e1d7b507ca0
foehammer127 <foehammer127@gmail.com>
2022-12-24 17:18:19 -0600

    Changed Theme to Doom-Challenger-Deep

--- init.el
+++ init.el
@@ -47,7 +47,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
+   '("512ce140ea9c1521ccaceaa0e73e2487e2d3826cc9d287275550b47c04072bc4" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
  '(package-selected-packages
    '(cider clojure-mode haskell-mode magit rust-mode vterm lsp-mode go-mode dashboard-hackernews dashboard hydra evil-collection evil helpful ivy-rich counsel ivy which-key doom-themes all-the-icons doom-modeline auto-package-update use-package))
  '(warning-suppress-types '((comp))))
@@ -113,7 +113,7 @@
 (global-set-key (kbd "C-c c") #'org-capture)
 
 (use-package doom-themes)
-(load-theme 'doom-Iosvkem)
+(load-theme 'doom-challenger-deep)
 
 (column-number-mode)
 (global-display-line-numbers-mode t)
